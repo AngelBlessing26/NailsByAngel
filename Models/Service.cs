af211c0b70d212404cead31e5cf26350947cae31
Your Name <your@email.com>
2025-12-15 21:38:57 +0000

    Booking page

--- Models/Service.cs
+++ Models/Service.cs
@@ -2,5 +2,10 @@
 {
     public class Service
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+
+        public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
     }
 }
